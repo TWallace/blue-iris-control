0ecbbd0d5f0ac8d75c3e96693ca6ca46e7dceffc
Tom <tomwallace75@hotmail.com>
2017-02-02 00:25:38 -0800

    Got login part working

---
+++ handlers/login.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var config = require('config-yml'),
+  http = require('request-promise'),
+  md5 = require('md5');
+
+module.exports = function() {
+  var options = {
+    method: 'POST',
+    uri: config.blueIrisUrl,
+    body: {
+      cmd: 'login'
+    },
+    json: true
+  };
+  return http.post(options)
+    .then(function(response) {
+      var session = response.session;
+      if (session) {
+        options.body.session = session;
+        options.body.response = md5(config.username
+          + ':' + session
+          + ':' + config.password);
+        return http.post(options)
+          .then(function(response) {
+            console.log(response);
+          });
+      }
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+};
--- handlers/trigger.js
+++ handlers/trigger.js
@@ -1,5 +1,18 @@
 'use strict';
 
-module.exports = function(req) {
+var config = require('config-yml'),
+  http = require('request-promise'),
+  login = require('./login.js'),
+  session;
 
+function trigger(response) {
+  return http.post(config.blueIrisUrl, {
+    cmd: 'trigger'
+  });
+}
+
+module.exports = function(req) {
+  return !session
+    ? login().then(trigger)
+    : trigger();
 };
