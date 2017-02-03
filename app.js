64aae629a0b045c7867b90e4ee3c282fe058056d
Tom Wallace <Tom.Wallace@nordstrom.com>
2017-02-03 10:23:49 -0800

    Added package.json, .jshintrc, .jscsrc

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 'use strict';
 
 var express = require('express'),
-  handlers = require('require-all')(__dirname + '/handlers'),
-  app = express();
+    handlers = require('require-all')(__dirname + '/handlers'),
+    app = express();
 
 app.set('port', process.env.port || 3000);
 
 app.post('/trigger', function(req, res) {
-  return handlers.trigger(req);
+    return handlers.trigger(req);
 });
 
 app.use(function(req, res) {
-  res.status(404).send('File Not Found');
+    res.status(404).send('File Not Found');
 });
 
 app.use(function(error, req, res) {
-  console.error(error.stack);
-  res.status(505).send('Internal Server Error');
+    console.error(error.stack);
+    res.status(505).send('Internal Server Error');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Example app listening on port 3000');
+    console.log('Example app listening on port 3000');
 });
--- handlers/trigger.js
+++ handlers/trigger.js
@@ -1,18 +1,18 @@
 'use strict';
 
 var config = require('config-yml'),
-  http = require('request-promise'),
-  login = require('./login.js'),
-  session;
+    http = require('request-promise'),
+    login = require('./login.js'),
+    session;
 
 function trigger(response) {
-  return http.post(config.blueIrisUrl, {
-    cmd: 'trigger'
-  });
+    return http.post(config.blueIrisUrl, {
+        cmd: 'trigger'
+    });
 }
 
 module.exports = function(req) {
-  return !session
-    ? login().then(trigger)
-    : trigger();
+    return !session
+        ? login().then(trigger)
+        : trigger();
 };
