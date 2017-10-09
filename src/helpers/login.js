a8b2ee41a95511ac997f599e973b924e03c36bf7
Tom <tomwallace75@hotmail.com>
2017-10-08 19:25:01 -0700

    Added additional error messaging.

--- src/helpers/login.js
+++ src/helpers/login.js
@@ -2,7 +2,9 @@
 
 var config = require('config-yml'),
   http = require('request-promise'),
-  md5 = require('./md5Hash.js');
+  md5 = require('./md5Hash.js'),
+  logger = require('../../util/logger.js')(module),
+  _ = require('lodash');
 
 module.exports = function () {
   var options = {
@@ -13,20 +15,25 @@ module.exports = function () {
     },
     json: true
   };
+
+  logger.info('Attempting to login to Blue Iris');
   return http.post(options)
-    .then(function (response) {
-      var session = response.session;
-      if (session) {
-        options.body.session = session;
-        options.body.response = md5(session);
-        return http.post(options)
-          .then(function (response) {
-            return response.session;
-          });
-      }
-      throw new Error('Could not login to Blue Iris');
-    })
-    .catch(function (error) {
-      throw error;
-    });
+  .then(function (response) {
+    if (response.result === 'fail') {
+      logger.error('Blue Iris login response: ' + JSON.stringify(response));
+      throw new Error('Could not login to Blue Iris: ' + response.data.reason);
+    }
+    var session = response.session;
+    if (session) {
+      options.body.session = session;
+      options.body.response = md5(session);
+      return http.post(options)
+      .then(function (response) {
+        return response.session;
+      });
+    }
+  })
+  .catch(function (error) {
+    throw error;
+  });
 };
